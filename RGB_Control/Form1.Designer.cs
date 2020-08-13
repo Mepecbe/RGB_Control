f0efb23786002a10d6905df8efcd8d559b979dc4
Никита <val0dko99@mail.ru>
2020-08-13 20:29:26 +0300

    Добавьте файлы проекта.

---
+++ RGB_Control/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace RGB_Control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(445, 16);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(94, 19);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "ПОДКЛЮЧЕН";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 352);
+            this.Controls.Add(this.metroLabel1);
+            this.Name = "Form1";
+            this.Text = "RGB Control";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+    }
+}
+
---
+++ RGB_Control/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace RGB_Control
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        SerialPort Serial = new SerialPort("COM5", 9600);
+
+        public Form1()
+        {
+            InitializeComponent();
+            Serial.Open();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            /*
+            Serial.WriteLine(colorDialog1.Color.R.ToString());
+            //Thread.Sleep(5);
+            Serial.WriteLine(colorDialog1.Color.G.ToString());
+            //Thread.Sleep(5);
+            Serial.WriteLine(colorDialog1.Color.B.ToString());*/
+            Serial.Write(new byte[] { colorDialog1.Color.R, colorDialog1.Color.G, colorDialog1.Color.B }, 0, 3);
+        }
+    }
+}
