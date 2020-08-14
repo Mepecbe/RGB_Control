c83bc7c77381464500ec58b422644633df05f51e
Никита <val0dko99@mail.ru>
2020-08-15 00:51:43 +0300

    Добавлена функция установки статичного цвета RGB ленты Работа над цветами в модели HSV

--- RGB_Control/Form1.Designer.cs
+++ RGB_Control/Form1.Designer.cs
@@ -28,36 +28,284 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.Label_State = new MetroFramework.Controls.MetroLabel();
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.colorWheel1 = new Cyotek.Windows.Forms.ColorWheel();
+            this.SetButton = new MetroFramework.Controls.MetroButton();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBox3 = new MetroFramework.Controls.MetroTextBox();
             this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroTabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // Label_State
+            // 
+            this.Label_State.AutoSize = true;
+            this.Label_State.Location = new System.Drawing.Point(425, 30);
+            this.Label_State.Name = "Label_State";
+            this.Label_State.Size = new System.Drawing.Size(114, 19);
+            this.Label_State.TabIndex = 0;
+            this.Label_State.Text = "НЕ ПОДКЛЮЧЕН";
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Controls.Add(this.tabPage1);
+            this.metroTabControl1.Controls.Add(this.tabPage2);
+            this.metroTabControl1.Controls.Add(this.tabPage3);
+            this.metroTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.metroTabControl1.Location = new System.Drawing.Point(20, 60);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.SelectedIndex = 1;
+            this.metroTabControl1.Size = new System.Drawing.Size(522, 272);
+            this.metroTabControl1.TabIndex = 1;
+            this.metroTabControl1.UseSelectable = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 38);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(514, 230);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Режимы";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.metroButton2);
+            this.tabPage2.Controls.Add(this.metroButton1);
+            this.tabPage2.Controls.Add(this.SetButton);
+            this.tabPage2.Controls.Add(this.colorWheel1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 38);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(514, 230);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Статический режим";
+            // 
+            // colorWheel1
+            // 
+            this.colorWheel1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.colorWheel1.Location = new System.Drawing.Point(283, 3);
+            this.colorWheel1.Name = "colorWheel1";
+            this.colorWheel1.Size = new System.Drawing.Size(228, 224);
+            this.colorWheel1.TabIndex = 0;
+            // 
+            // SetButton
+            // 
+            this.SetButton.Location = new System.Drawing.Point(15, 10);
+            this.SetButton.Name = "SetButton";
+            this.SetButton.Size = new System.Drawing.Size(135, 40);
+            this.SetButton.TabIndex = 1;
+            this.SetButton.Text = "Установить";
+            this.SetButton.UseSelectable = true;
+            this.SetButton.Click += new System.EventHandler(this.SetButton_Click);
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(82, 136);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(75, 23);
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Text = "+1";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(94, 182);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(75, 23);
+            this.metroButton2.TabIndex = 3;
+            this.metroButton2.Text = "-1";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.metroButton3);
+            this.tabPage3.Controls.Add(this.metroLabel3);
+            this.tabPage3.Controls.Add(this.metroLabel2);
+            this.tabPage3.Controls.Add(this.metroLabel1);
+            this.tabPage3.Controls.Add(this.metroTextBox3);
+            this.tabPage3.Controls.Add(this.metroTextBox2);
+            this.tabPage3.Controls.Add(this.metroTextBox1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 38);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(514, 230);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Цвет в HSV";
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(53, 1);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.Lines = new string[0];
+            this.metroTextBox1.Location = new System.Drawing.Point(19, 14);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(75, 23);
+            this.metroTextBox1.TabIndex = 0;
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroTextBox2
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox2.CustomButton.Image = null;
+            this.metroTextBox2.CustomButton.Location = new System.Drawing.Point(53, 1);
+            this.metroTextBox2.CustomButton.Name = "";
+            this.metroTextBox2.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox2.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox2.CustomButton.TabIndex = 1;
+            this.metroTextBox2.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox2.CustomButton.UseSelectable = true;
+            this.metroTextBox2.CustomButton.Visible = false;
+            this.metroTextBox2.Lines = new string[0];
+            this.metroTextBox2.Location = new System.Drawing.Point(20, 59);
+            this.metroTextBox2.MaxLength = 32767;
+            this.metroTextBox2.Name = "metroTextBox2";
+            this.metroTextBox2.PasswordChar = '\0';
+            this.metroTextBox2.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox2.SelectedText = "";
+            this.metroTextBox2.SelectionLength = 0;
+            this.metroTextBox2.SelectionStart = 0;
+            this.metroTextBox2.ShortcutsEnabled = true;
+            this.metroTextBox2.Size = new System.Drawing.Size(75, 23);
+            this.metroTextBox2.TabIndex = 1;
+            this.metroTextBox2.UseSelectable = true;
+            this.metroTextBox2.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox2.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroTextBox3
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox3.CustomButton.Image = null;
+            this.metroTextBox3.CustomButton.Location = new System.Drawing.Point(53, 1);
+            this.metroTextBox3.CustomButton.Name = "";
+            this.metroTextBox3.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox3.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox3.CustomButton.TabIndex = 1;
+            this.metroTextBox3.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox3.CustomButton.UseSelectable = true;
+            this.metroTextBox3.CustomButton.Visible = false;
+            this.metroTextBox3.Lines = new string[0];
+            this.metroTextBox3.Location = new System.Drawing.Point(20, 109);
+            this.metroTextBox3.MaxLength = 32767;
+            this.metroTextBox3.Name = "metroTextBox3";
+            this.metroTextBox3.PasswordChar = '\0';
+            this.metroTextBox3.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox3.SelectedText = "";
+            this.metroTextBox3.SelectionLength = 0;
+            this.metroTextBox3.SelectionStart = 0;
+            this.metroTextBox3.ShortcutsEnabled = true;
+            this.metroTextBox3.Size = new System.Drawing.Size(75, 23);
+            this.metroTextBox3.TabIndex = 2;
+            this.metroTextBox3.UseSelectable = true;
+            this.metroTextBox3.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox3.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
             // 
             // metroLabel1
             // 
             this.metroLabel1.AutoSize = true;
-            this.metroLabel1.Location = new System.Drawing.Point(445, 16);
+            this.metroLabel1.Location = new System.Drawing.Point(121, 15);
             this.metroLabel1.Name = "metroLabel1";
-            this.metroLabel1.Size = new System.Drawing.Size(94, 19);
-            this.metroLabel1.TabIndex = 0;
-            this.metroLabel1.Text = "ПОДКЛЮЧЕН";
+            this.metroLabel1.Size = new System.Drawing.Size(18, 19);
+            this.metroLabel1.TabIndex = 3;
+            this.metroLabel1.Text = "H";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(121, 62);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(16, 19);
+            this.metroLabel2.TabIndex = 4;
+            this.metroLabel2.Text = "S";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(119, 110);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(17, 19);
+            this.metroLabel3.TabIndex = 5;
+            this.metroLabel3.Text = "V";
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Location = new System.Drawing.Point(181, 58);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(75, 23);
+            this.metroButton3.TabIndex = 6;
+            this.metroButton3.Text = "metroButton3";
+            this.metroButton3.UseSelectable = true;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(562, 352);
-            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroTabControl1);
+            this.Controls.Add(this.Label_State);
             this.Name = "Form1";
             this.Text = "RGB Control";
+            this.metroTabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ColorDialog colorDialog1;
+        private MetroFramework.Controls.MetroLabel Label_State;
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private Cyotek.Windows.Forms.ColorWheel colorWheel1;
+        private MetroFramework.Controls.MetroButton SetButton;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
         private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox3;
+        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
     }
 }
 
--- RGB_Control/Form1.cs
+++ RGB_Control/Form1.cs
@@ -1,30 +1,130 @@
-﻿using System;
+﻿using MetroFramework;
+using System;
 using System.IO.Ports;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using Cyotek.Windows.Forms;
 
 namespace RGB_Control
 {
     public partial class Form1 : MetroFramework.Forms.MetroForm
     {
+        byte[] rgb = new byte[3]; 
         SerialPort Serial = new SerialPort("COM5", 9600);
-
+        
         public Form1()
         {
             InitializeComponent();
-            Serial.Open();
+
+            try
+            {
+                Serial.Open();
+                Label_State.Text = "ПОДКЛЮЧЕН";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка открытия порта\n " + ex.Message);
+
+                new Task(() => 
+                {
+                    while (true) {
+                        try
+                        {
+                            Serial.Open();
+                            Label_State.Text = "ПОДКЛЮЧЕН";
+                            break;
+                        } 
+                        catch 
+                        {
+
+                        }
+
+                        Thread.Sleep(TimeSpan.FromSeconds(3));
+                    }
+                }).Start();
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void SetButton_Click(object sender, EventArgs e)
         {
-            colorDialog1.ShowDialog();
+            rgb = new byte[]{
+                colorWheel1.Color.R,
+                colorWheel1.Color.G,
+                colorWheel1.Color.B  
+            };
+            
+            Serial.Write(rgb, 0, 3);
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            //+1
+            byte value = 10;
+
+            if (rgb[0] + 10 <= 255)
+                rgb[0] += value;
+
+            if (rgb[1] + 10 <= 255)
+                rgb[1] += value;
+
+            if (rgb[2] + 10 <= 255)
+                rgb[2] += value;
+
+
+            Serial.Write(rgb, 0, 3);
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            //-1
+            byte value = 10;
+
+            if(rgb[0]-10 > 0)
+                rgb[0] -= value;
+
+            if (rgb[1] > 0)
+                rgb[1] -= value;
+
+            if (rgb[2] > 0)
+                rgb[2] -= value;
+
+            Serial.Write(rgb, 0, 3);
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            int H = int.Parse(metroTextBox1.Text);
+            byte S = byte.Parse(metroTextBox2.Text);
+            byte V = byte.Parse(metroTextBox3.Text);
+
+            float Hi = H / 60 % 6 ;
+            float Vmin = ((100 - S) * V) / 100;
+            float a = V - Vmin * ((H % 60) / 60);
+
+            byte Vinc  = Convert.ToByte((Vmin + a) * 255/100);
+            byte Vdec = Convert.ToByte((V - a) * 255 / 100);
             /*
-            Serial.WriteLine(colorDialog1.Color.R.ToString());
-            //Thread.Sleep(5);
-            Serial.WriteLine(colorDialog1.Color.G.ToString());
-            //Thread.Sleep(5);
-            Serial.WriteLine(colorDialog1.Color.B.ToString());*/
-            Serial.Write(new byte[] { colorDialog1.Color.R, colorDialog1.Color.G, colorDialog1.Color.B }, 0, 3);
+            switch (Convert.ToByte(Hi))
+            {
+                case 0: { rgb = new byte[] { V, Vinc, Convert.ToByte(Vmin * 255 / 100) };  break; }
+                case 1: { rgb = new byte[] { Vdec, V, Convert.ToByte(Vmin * 255 / 100) }; break; }
+                case 2: { rgb = new byte[] { Convert.ToByte(Vmin * 255 / 100), V, Vinc }; break; }
+                case 3: { rgb = new byte[] { Convert.ToByte(Vmin * 255 / 100), Vdec, V }; break; }
+                case 4: { rgb = new byte[] { Vinc, Convert.ToByte(Vmin * 255 / 100), V }; break; }
+                case 5: { rgb = new byte[] { V, Convert.ToByte(Vmin * 255 / 100), Vdec }; break; }
+            }*/
+
+            rgb = new byte[] { V, Vinc, Convert.ToByte(Vmin) };
+
+            {
+                MessageBox.Show($" r {rgb[0]} \n" +
+                                $" g {rgb[1]}\n" +
+                                $" b {rgb[2]}");
+
+                Serial.Write(rgb, 0, 3);
+            }
         }
     }
 }
